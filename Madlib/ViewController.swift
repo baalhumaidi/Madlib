31cf2cb9b86ba61ea830680db0a7c5ea60c92912
baalhumaidi <93419731+baalhumaidi@users.noreply.github.com>
2021-12-12 08:34:07 +0000

    Initial Commit

---
+++ Madlib/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Madlib
+//
+//  Created by admin on 12/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var thephrase: UILabel!
+
+var array = [String]()
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        thephrase.isHidden = true
+    
+//
+        print (array)
+    }
+
+
+    @IBAction func movenextpage(_ sender: UIBarButtonItem) {
+       performSegue(withIdentifier: "next", sender: self)
+    
+    }
+
+    @IBAction func unwind(_ segue: UIStoryboardSegue)    {
+        
+        //thephrase.text = array[String]
+        thephrase.isHidden = false
+      thephrase.text = "we are having a \(array[0]) (crazy) time now. Later we (will) \(array[1]) run and \(array[2]) (swim) in the \(array[3]) (basement)"
+   
+    
+    }
+
+ 
+}
+    
---
+++ Madlib/secViewController.swift
@@ -0,0 +1,64 @@
+//
+//  secViewController.swift
+//  Madlib
+//
+//  Created by admin on 12/12/2021.
+//
+
+import UIKit
+
+class secViewController: UIViewController {
+
+    
+    @IBOutlet weak var adjective: UITextField!
+    
+    @IBOutlet weak var verb: UITextField!
+    
+    @IBOutlet weak var verb2: UITextField!
+    
+    @IBOutlet weak var noun: UITextField!
+    
+    
+    @IBOutlet var first: [UITextField]!
+    
+   var arr = [String]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    
+         let gotomain = segue.destination as! ViewController
+       
+        gotomain.array = [adjective.text!,verb.text!,verb2.text!,noun.text!]  }
+   
+  
+    }
+    
+  
+        
+
+        
+
+           //     performSegue(withIdentifier: <#T##String#>, sender: <#T##Any?#>)
+
+    
+
+    /*
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+              direction = sender as! String
+             let dest = segue.destination as! direction
+         dest.direction = direction
+         }
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
