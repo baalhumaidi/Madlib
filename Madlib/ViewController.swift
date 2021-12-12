6bfac1222b41f1bdd0a42e85d2257bd573d49d9c
baalhumaidi <93419731+baalhumaidi@users.noreply.github.com>
2021-12-12 16:58:54 +0000

    ...

--- Madlib/ViewController.swift
+++ Madlib/ViewController.swift
@@ -16,7 +16,7 @@ var array = [String]()
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        thephrase.isHidden = true
+        thephrase.text = "..."
     
 //
         print (array)
@@ -31,7 +31,7 @@ var array = [String]()
     @IBAction func unwind(_ segue: UIStoryboardSegue)    {
      // is therre a best way to do this???
         //thephrase.text = array[String]
-        thephrase.isHidden = false
+        
       thephrase.text = "we are having a \(array[0]) (crazy) time now. Later we (will) \(array[1]) run and \(array[2]) (swim) in the \(array[3]) (basement)"
    
     
